4bced1beee08e11755e2e0965a539e6f445e7c53
oksanaturii <o.turij@gmail.com>
2024-07-11 12:28:41 -0700

    Add ymlFile

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,8 +1,16 @@
 package utils;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Utils {
@@ -17,6 +25,18 @@ public class Utils {
 
     public static void loginPage(WebDriver driver) {
         driver.get("https://www.saucedemo.com/");
+    }
+
+    public static void takeScreenshot(WebDriver driver, String methodName, String className) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy_MM_dd_hhmmss");
+        String date = simpleDateFormat.format(new Date());
+
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File(String.format("screenshots/screenshot_%s_%s_%s.png", className, methodName, date)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
