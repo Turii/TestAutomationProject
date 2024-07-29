292a29d772256aeb757dd294bb33d1f7e52c5a65
oksanaturii <o.turij@gmail.com>
2024-07-29 14:18:04 -0700

    edit command to ci to run UnitTests

--- src/test/java/tests/AnimalUtilsTest.java
+++ src/test/java/tests/AnimalUtilsTest.java
@@ -8,7 +8,7 @@ public class AnimalUtilsTest {
     @Test
     public void testSum() {
         System.out.println("Test");
-        assertEquals(5, AnimalUtils.sum(2, 3));
+        assertEquals(8, AnimalUtils.sum(2, 3));
         assertEquals(-1, AnimalUtils.sum(-2, 1));
         assertEquals(0, AnimalUtils.sum(0, 0));
     }
