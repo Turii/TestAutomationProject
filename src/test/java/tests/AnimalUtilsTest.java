8da84ed61d8dbf8fa942333c68349fcbc72ddd72
oksanaturii <o.turij@gmail.com>
2024-07-29 13:56:03 -0700

    edit command to ci to run UnitTests

--- src/test/java/tests/AnimalUtilsTest.java
+++ src/test/java/tests/AnimalUtilsTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AnimalUtilsTest {
     @Test
     public void testSum() {
+        System.out.println("Test");
         assertEquals(5, AnimalUtils.sum(2, 3));
         assertEquals(-1, AnimalUtils.sum(-2, 1));
         assertEquals(0, AnimalUtils.sum(0, 0));
